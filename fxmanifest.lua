f1ba03f783daa8ff6e12e7de24b68f6163275d36
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-01-31 17:58:18 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Polished SEM Interaction Menu with Controller & NUI'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
